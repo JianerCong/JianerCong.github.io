01d83737228ad957d77213f81ca1fef896620c5d
Jianer <congjianer@outlook.com>
2022-08-30 10:00:54 +0100

    Init

---
+++ js/about.dc31b76b.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkhi"]=self["webpackChunkhi"]||[]).push([[443],{7381:function(n,u,t){t.r(u),t.d(u,{default:function(){return f}});var a=t(3396);const e={class:"about"},r=(0,a._)("h1",null,"This is an about page",-1),s=[r];function i(n,u){return(0,a.wg)(),(0,a.iD)("div",e,s)}var c=t(89);const h={},o=(0,c.Z)(h,[["render",i]]);var f=o}}]);
+//# sourceMappingURL=about.dc31b76b.js.map
